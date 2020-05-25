46bc20d93b6a4c31cca9b2635ea591f6dcd9a486
冷有暖 <1304271496@qq.com>
2020-05-25 10:33:35 +0800

    模拟SpringMVC基本实现流程

---
+++ src/com/gthree/test/api/UserController.java
@@ -0,0 +1,36 @@
+package com.gthree.test.api;
+
+import com.gthree.gtmvcframework.annotation.GtAutowired;
+import com.gthree.gtmvcframework.annotation.GtController;
+import com.gthree.gtmvcframework.annotation.GtRequestMapping;
+import com.gthree.gtmvcframework.annotation.GtRequestParam;
+import com.gthree.test.entity.User;
+import com.gthree.test.service.IUserService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+//http://localhost:8081/GtMVC_war_exploded/user/users?name=zhangsan&age=111
+@GtController
+@GtRequestMapping("/user")
+public class UserController {
+
+    @GtAutowired
+    private IUserService userService;
+
+    @GtRequestMapping("/users")
+    public void getAllUsers(@GtRequestParam(value = "name") String name, @GtRequestParam(value = "age") Integer age, HttpServletResponse response) throws IOException {
+        System.out.println("接受请求参数:" + name + age);
+        List<User> users = userService.findAllUsers();
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write(users.toString());
+        response.getWriter().println("你就是个弟弟");
+
+    }
+
+    @GtRequestMapping("/deps")
+    public void getAllUsers(){
+        System.out.println("查询部门");
+    }
+}
